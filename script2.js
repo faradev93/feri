ebab797e4651bd54e2e20d19e05f1887d39c0dd7
faradev93 <faramarz.rezakhanlou.sf@gmail.com>
2024-09-09 23:13:27 +0330

    ro object bishtar kar beshe:))))

---
+++ script2.js
@@ -0,0 +1,18 @@
+let pedar = { name: "faraz", "Last Name": "RK", Female: false, "function(){}": "No", Pedarsag: "yes" };
+console.log(Object.keys(pedar));
+console.log(Object.values(pedar));
+console.log(Object.entries(pedar));
+console.log(Object.entries(pedar).length)
+
+
+for (oo = -1;oo<=2;oo++ ) 
+    {
+        
+        console.log(oo);
+     }
+     //LOOOOP//
+for (oo = -4;oo<=1;oo++ ) 
+{
+    console.log(oo);
+    console.log(typeof oo);
+ }
