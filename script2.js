4ce5126e085ca5e4746a45918f0d9e034ba54a5a
faradev93 <faramarz.rezakhanlou.sf@gmail.com>
2024-09-10 00:10:17 +0330

    ta inja s02

--- script2.js
+++ script2.js
@@ -5,25 +5,26 @@ console.log(Object.entries(pedar));
 console.log(Object.entries(pedar).length)
 
 
-for (oo = -1;oo<=2;oo++ ) 
-    {
-        
-        console.log(oo);
-     }
-     //LOOOOP//
-for (oo = -4;oo<=1;oo++ ) 
-{
+for (oo = -1; oo <= 2; oo++) {
+
+    console.log(oo);
+}
+//LOOOOP//
+for (oo = -4; oo <= 1; oo++) {
     console.log(oo);
     console.log(typeof oo);
- }
+}
 
- let typer=['hasan',1,2,5,true,"a",'String',function() {}];
+typer(["hasan", 1, 2, 5, true, "a", 'String', function () { }]);
 
- function dastan(){
+console.log(typeof typer);
+function typer(golabi) {
+    for(w=0;w<=typer.length;w++)
+    {console.log(golabi.length);
 
-console.log(golabi);
- }
- 
+    }
+    console.log(golabi);
+}
 //  let typer=['hasan',1,2,5,true,"a",'String',function() {}];
 //  for(input=0, input<=typer; input++;){
 //     console.log(typeof typer[input]);
