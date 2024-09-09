ae96800a1b78b49867aac65dcb139998b3444a82
faradev93 <faramarz.rezakhanlou.sf@gmail.com>
2024-09-09 19:20:14 +0330

    bazam test

---
+++ script.js
@@ -0,0 +1,11 @@
+let golabi = "faramarz2";
+let age = 20;
+let ghazanfar = ["faramarz", false, 30, golabi, [1, 2, 3]]
+
+// console.log(ghazanfar);
+// console.log(ghazanfar.length);
+// console.log(typeof ghazanfar);
+for(index=0; index<=ghazanfar.length;index++){
+    console.log(ghazanfar[index]);
+}
+console.log(Array.isArray(ghazanfar));
