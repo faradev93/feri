8073bbebf79d8f94dcb6b517d33ef43f84c5d1a5
faradev93 <faramarz.rezakhanlou.sf@gmail.com>
2024-09-10 21:26:11 +0330

    javab dorost

---
+++ script3.js
@@ -0,0 +1,32 @@
+let golabi2 = 35 + 5;
+let myArray = [1, "faramarz", golabi2, function () { }, [10, 20], null, false];
+console.log(myArray[1]);
+console.log(myArray.length);
+console.log(Array.isArray(myArray));
+console.log(typeof myArray);
+
+for (let k = 0; k <= myArray; k++) {
+    console.log(typeof myArray[k]);
+}
+
+
+//Object//
+let monpel = { name: "faramarz", age: golabi2, sexM: true, dau: false, reshte: "string" };
+console.log(Object.keys(monpel));
+console.log(Object.values(monpel));
+console.log(Object.entries(monpel));
+
+
+
+
+
+
+function typer(input) {
+    for (a = 0; a < input.length; a++) {
+        console.log(typeof input[a])
+    }
+}
+
+
+typer(["hasan", 1, 2, 5, true, "a", 'String', function () { }]);
+console.log(Array.isArray(typer));
